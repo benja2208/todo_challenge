2e2b290358c10298bb4a85f315a61dda9ac07771
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-17 16:37:00 +0100

    can add items

--- public/js/toDoController.js
+++ public/js/toDoController.js
@@ -1,3 +1,15 @@
 toDo.controller('ToDoController', [function() {
+	var self = this; 
+
+	self.tasks = [];
+
+	self.addTask = function() {
+    self.tasks.push(
+      { 
+        "name" : self.taskName,
+        "completed": false
+      });
+    self.taskName = ''
+  };
 
 }]);
--- test/toDoController.spec.js
+++ test/toDoController.spec.js
@@ -3,11 +3,28 @@ describe('ToDoController', function() {
 
   var ctrl;
 
+  var tasks
+
   beforeEach(inject(function($controller) {
     ctrl = $controller('ToDoController');
   }));
 
+  beforeEach(inject(function(ToDoItems){
+    tasks = toDoItems;
+  }));
+
   it('initialises with an empty to do field', function() {
-    expect(ctrl.toDoItem).toBeUndefined();
+    expect(ctrl.newItem).toBeUndefined();
+  });
+
+  it('can add a task', function(){
+  	ctrl.newItem = "first task"
+    ctrl.addTask();
+    expect(ctrl.tasks).toEqual([
+      {
+        'name': 'Adding task number 1',
+        'Completed': false
+      }
+    ]);
   });
 });
