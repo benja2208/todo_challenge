d88eea5dbc9316bc54bf2c6910dd65b88459d8fb
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-17 17:27:12 +0100

    delete with test work

--- public/js/toDoController.js
+++ public/js/toDoController.js
@@ -7,9 +7,19 @@ toDo.controller('ToDoController', [function() {
     self.tasks.push(
       { 
         "name" : self.taskName,
+        "date" : self.taskDate,
         "completed": false
       });
     self.taskName = ''
+    self.taskDate = ''
+  };
+
+  self.deleteTask = function(task){
+    self.tasks.splice(task,1);
+  };
+
+  self.editTask = function(){
+
   };
 
 }]);
--- test/toDoController.spec.js
+++ test/toDoController.spec.js
@@ -9,22 +9,32 @@ describe('ToDoController', function() {
     ctrl = $controller('ToDoController');
   }));
 
-  beforeEach(inject(function(ToDoItems){
-    tasks = toDoItems;
-  }));
-
-  it('initialises with an empty to do field', function() {
-    expect(ctrl.newItem).toBeUndefined();
+  it('initialises with an empty name field', function() {
+    expect(ctrl.taskName).toBeUndefined();
   });
 
-  it('can add a task', function(){
-  	ctrl.newItem = "first task"
+  it('initialises with an empty date field', function() {
+    expect(ctrl.taskDate).toBeUndefined();
+  });
+
+
+  it('can add a task name', function(){
+  	ctrl.taskName = "first task" ;
     ctrl.addTask();
-    expect(ctrl.tasks).toEqual([
-      {
-        'name': 'Adding task number 1',
-        'Completed': false
-      }
-    ]);
+    expect(ctrl.tasks[0].name).toEqual("first task")
+  });
+
+  it('can add a task name', function(){
+    ctrl.taskDate = "Thu Sep 10 2015 00:00:00 GMT+0100 (BST)" ;
+    ctrl.addTask();
+    expect(ctrl.tasks[0].date).toEqual("Thu Sep 10 2015 00:00:00 GMT+0100 (BST)")
+  }); 
+
+  it('can delete tasks', function(){
+    ctrl.taskName = "first task" ;
+    ctrl.taskDate = "Thu Sep 10 2015 00:00:00 GMT+0100 (BST)" ;
+    ctrl.addTask();
+    ctrl.deleteTask();
+    expect(ctrl.tasks).toBeUndefined;
   });
 });
